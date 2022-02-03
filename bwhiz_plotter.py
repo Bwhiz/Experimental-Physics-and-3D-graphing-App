ba1bc482f28dd4589fcd2471d0c0ee3a40f8b013
Benedict O. Ejelonu <59194091+Bwhiz@users.noreply.github.com>
2022-02-03 11:13:02 +0100

    Update bwhiz_plotter.py

--- bwhiz_plotter.py
+++ bwhiz_plotter.py
@@ -11,6 +11,14 @@ st.set_page_config(
     layout= "wide"
 )
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 def main():
     page = st.sidebar.selectbox(
         "Select task to perform",
